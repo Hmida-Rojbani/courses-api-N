309d5e8284bd0f30568f0b1c00f019d0404638d6
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-03-25 20:24:40 +0100

    post

--- routers/courses.js
+++ routers/courses.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const Course = require('../models/course');
+const _ = require('lodash')
 
+router.get('', async (req,res) =>{
+    let courses = await Course.find();
 
+    res.send(courses)
+})
+
+router.post('', async (req,res) =>{
+    let course = new Course(_.pick(req.body,['title','author','tags','price','isPublished']));
+    await course.save();
+    res.status(201).send(course)
+})
 
 module.exports = router;

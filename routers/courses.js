62145576537e39b2a35bf2e4e5b9b445e52599f6
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-01 19:09:10 +0100

    find

--- routers/courses.js
+++ routers/courses.js
@@ -13,5 +13,41 @@ router.post('', async (req,res) =>{
     await course.save();
     res.status(201).send(course)
 })
+// by Id
+router.get('/id/:id', async (req,res) =>{
+    try {
+        var course = await Course.findById(req.params.id);
+    } catch (error) {
+        return res.status(400).send('Unaccepted Id');
+    }
+    if(!course)
+        return res.status(404).send('Course with this id is not found');
+    res.send(course)
+})
+
+
+// by title
+router.get('/title/:title', async (req,res) =>{
+
+        var courses = await Course.find({title : { $regex : '^'+req.params.title}});
+
+    if(!courses)
+        return res.status(404).send('Course with this title is not found');
+    res.send(courses)
+})
+// with price in bounds
+// eq (==) ; neq (!=)
+// gt (>)  ; gte (>=)
+// lt (<)  ; lte (<=)
+// in : [ e1 , e2 ]  ||  nin  : [ e1 , e2 ]
+router.get('/price/:min/:max', async (req,res) =>{
+
+    var courses = await Course.find({price : { $lte : req.params.max,
+        $gte : req.params.min  }});
+
+if(!courses)
+    return res.status(404).send('There are no courses between the given two prices');
+res.send(courses)
+});
 
 module.exports = router;
